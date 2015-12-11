bddf5e2ec8cde224334ba75f7b897135de94cbb0
Raymond Lee <raymond+github@caltech.edu>
2015-12-10 16:14:09 -0800

    To make a dictionary after removing parent terms whose children have already been completely accounted for. Thus to reduce redundancy.

--- src/generate_wbbt_dictionary_RemoveParentIfChildrenComplete.pl
+++ src/generate_wbbt_dictionary_RemoveParentIfChildrenComplete.pl
@@ -3,7 +3,20 @@
 
 # generate dictionary of genes to anatomy terms based on
 # http://wormbase.caltech.edu:8080/wormbase/manual/geneenrichment/davidahypergeometrictest
-## variant that eliminates anatomy terms that have all children already in the list to remove some redundancy
+# eliminates anatomy terms that have all children (with some annotation) already in the list to remove some redundancy
+
+
+
+
+# we're generating KEEP and using that.  We have GOOD and we're never
+# adding nor removing from it.  DISCARD we're keeping just in case we
+# want to look at it later.
+# Terms only go in KEEP if it either has a transitive term not in GOOD,
+# or no transitive terms at all.
+# Reading the regulates_closure counts into a %regulatesCount hash.
+# Then when looping through GOOD, if an edge has a sub with a count of
+# zero, it gets ignored (the edge gets ignored as if it was never
+# there).
 
 use CGI;
 use strict;
@@ -21,35 +34,73 @@ my $page_data = get $url;
 my $perl_scalar = $json->decode( $page_data );
 my %jsonHash = %$perl_scalar;
 
-## Fetch anatomy terms that have sufficient number (specified in solr query, $url) of annotated genes
+## Fetch anatomy terms that have sufficient number (specified in solr query above, $url) of annotated genes
 my $arrRef = $jsonHash{"facet_counts"}{"facet_fields"}{"regulates_closure"} ;
 my @array = @$arrRef;
 my %wbbt;
+my %good;
+my %keep;
+my %discard;
 while (@array) {
   my $wbbt = shift @array;
   my $count = shift @array;
   $wbbt{$wbbt} = $count;
+  $good{$wbbt} = $count;
 }
 
-## Remove terms from list if any of its descendants is in the list
-=comment out
+## Fetch 
+my %regulatesCount;
+my $urlRegulates = $solr_url . "select?qt=standard&indent=on&wt=json&version=2.2&fl=id&start=0&rows=0&q=document_category:bioentity&facet=true&facet.field=regulates_closure&facet.limit=-1&facet.sort=count&fq=source:%22WB%22&fq=-qualifier:%22not%22";
+my $pageRegulates_data = get $urlRegulates;
+my $perlRegulates_scalar = $json->decode( $pageRegulates_data );
+my %jsonHashRegulates = %$perlRegulates_scalar;
+my $arrRefRegulates = $jsonHashRegulates{'facet_counts'}{'facet_fields'}{'regulates_closure'};
+my @arrayRegulates = @$arrRefRegulates;
+
+
+while (scalar @arrayRegulates > 0) {
+   my $id    = shift @arrayRegulates;
+   my $count = shift @arrayRegulates;
+   $regulatesCount{$id} = $count;
+}
+
 foreach my $wbbt (sort { $wbbt{$a} <=> $wbbt{$b} } keys %wbbt) { 
-  next unless ($wbbt{$wbbt});				# terms removed while looping, so skip those already removed
-#  print qq($wbbt\t$wbbt{$wbbt}\n);
-  my $url = $solr_url . "select?qt=standard&fl=regulates_closure&version=2.2&wt=json&indent=on&rows=1&q=id:%22" . $wbbt . "%22&fq=document_category:%22ontology_class%22";
+  my $url = $solr_url . "select?qt=standard&fl=topology_graph_json&version=2.2&wt=json&indent=on&rows=1&q=id:%22" . $wbbt . "%22&fq=document_category:%22ontology_class%22";
   my $page_data = get $url;
   my $perl_scalar = $json->decode( $page_data );
   my %jsonHash = %$perl_scalar;
-  my $arrRef = $jsonHash{'response'}{'docs'}[0]{'regulates_closure'};
-  my @array = @$arrRef;
-  foreach my $other (@array) {
-    if ( ($other ne $wbbt) && ($wbbt{$other}) ) { delete $wbbt{$other}; } }	# if other term is in list, remove from list
-} 
+  my $topo_data = $json->decode( $jsonHash{"response"}{"docs"}[0]{"topology_graph_json"} );
+  my %topo = %$topo_data;
+  my (@edges) = @{ $topo{"edges"} };
+  my $hasTransChild = 0;
+  my $hasTransChildNotGood = 0;
+  for my $index (0 .. @edges) {
+    my ($sub, $obj, $pred) = ('', '', '');
+    my $isTrans = 0;
+    if ($edges[$index]{'sub'}) {  $sub  = $edges[$index]{'sub'};  }
+    if ($edges[$index]{'obj'}) {  $obj  = $edges[$index]{'obj'};  }
+    if ($edges[$index]{'pred'}) { $pred = $edges[$index]{'pred'}; }
+    next unless ($regulatesCount{$sub});
+    if ( ($pred eq 'is_a') || ($pred eq 'part_of') || ($pred eq 'occurs_in') || ($pred =~ m/.*regulates/) ) { $isTrans++; }
+    if ( ($isTrans) && ($obj eq $wbbt) ) { 
+      unless ($good{$sub}) { $hasTransChildNotGood++; }
+      $hasTransChild++; }
+  }
+  if ($hasTransChildNotGood) { $keep{$wbbt}++;    }
+    elsif ($hasTransChild) {   $discard{$wbbt}++; }
+    else {                     $keep{$wbbt}++;    }
+}
+
+=for testing
+foreach my $wbbt (sort keys %discard) { print qq(DISCARD $wbbt DISC\n); }
+foreach my $wbbt (sort keys %keep) { print qq(KEEP $wbbt KEEP\n); }
+foreach my $wbbt (sort keys %wbbt) { print qq(GOOD $wbbt GOOD\n); }
 =cut
 
-## Fetch genes annotated to each anatomy term
+
+## Fetch genes annotated to each KEEP anatomy term
 my %genes;
-foreach my $wbbt (sort keys %wbbt) {
+foreach my $wbbt (sort keys %keep) {
   my $url = $solr_url . "select?qt=standard&indent=on&wt=json&version=2.2&fl=id&start=0&rows=10000&q=document_category:bioentity&fq=source:%22WB%22&fq=-qualifier:%22not%22&fq=regulates_closure:%22" . $wbbt . "%22";
   my $page_data = get $url;
   my $perl_scalar = $json->decode( $page_data );
@@ -81,7 +132,7 @@ foreach my $hashRef (@array) {
 
 ## Print dictionary in csv
 my @wbbts_header;
-foreach my $wbbt (sort keys %wbbt) {
+foreach my $wbbt (sort keys %keep) {
   push @wbbts_header, qq($wbbtName{$wbbt}($wbbt));
 }
 my $wbbts_header = join",", @wbbts_header;
@@ -89,7 +140,7 @@ my $wbbts_header = join",", @wbbts_header;
 print qq(wbid,$wbbts_header\n);
 foreach my $gene (sort keys %genes) {
   my @out;
-  foreach my $wbbt (sort keys %wbbt) {
+  foreach my $wbbt (sort keys %keep) {
     if ($genes{$gene}{$wbbt}) { push @out, '1'; } else { push @out, '0'; } }
   my $out = join",", @out;
   print qq($gene,$out\n);
