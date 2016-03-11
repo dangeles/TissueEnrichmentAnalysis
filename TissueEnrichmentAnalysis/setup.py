eba5c03daf5b4d30b7c8afa4ae01263e3a486272
David Angeles <dangeles@caltech.edu>
2016-03-11 13:01:57 -0800

    Preparing for PyPI live
    I’m changing some of the file structures and adding setup.py scripts to
    prepare to make TEA ready for use via pip install

---
+++ TissueEnrichmentAnalysis/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 11:27:39 2016
+
+@author: dangeles
+"""
+
+from distutils.core import setup
+setup(
+  name = 'TEA',
+  packages = ['TEA'], # this must be the same as the name above
+  version = '0.1',
+  description = 'This package contains all the software used to implement TEA in WormBase and remotely',
+  author = 'David Angeles-Albores',
+  author_email = 'dangeles@caltech.edu',
+  url = 'https://github.com/dangeles/TissueEnrichmentAnalysis', # use the URL to the github repo
+  download_url = 'https://github.com/dangeles/TissueEnrichmentAnalysis/tarball/0.1', # I'll explain this in a second
+  keywords = ['tissue enrichment', 'celegans', 'biology'], # arbitrary keywords
+  classifiers = [],
+)
+
