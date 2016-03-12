9f342e4c7a847f26b22b7d5372b40cfb58eadebd
David Angeles <dangeles@caltech.edu>
2016-03-11 17:43:49 -0800

    updated version.

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ from distutils.core import setup
 setup(
   name = 'tissue_enrichment_analysis',
   packages = ['tissue_enrichment_analysis'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.12',
   description = 'This package contains all the software used to implement TEA in WormBase and remotely',
   author = 'David Angeles-Albores',
   author_email = 'dangeles@caltech.edu',
   url = 'https://github.com/dangeles/TissueEnrichmentAnalysis', # use the URL to the github repo
-  download_url = 'https://github.com/dangeles/TissueEnrichmentAnalysis/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/dangeles/TissueEnrichmentAnalysis/tarball/0.12', # I'll explain this in a second
   keywords = ['tissue enrichment', 'celegans', 'biology'], # arbitrary keywords
   classifiers = [],
 )
